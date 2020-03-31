6936069e1bf154144cba1cdebda5218f0b2e1172
Ankith Madadi <ankithm28@gmail.com>
2020-03-30 21:30:47 -0700

    View Controller

--- ShapeFit/ShapeFit/ViewControllers/ScreenViewController.swift
+++ ShapeFit/ShapeFit/ViewControllers/ScreenViewController.swift
@@ -9,5 +9,20 @@
 import UIKit
 
 class ScreenViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        GCHelper.sharedInstance.authenticateLocalUser()
+    }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        AppCache.instance.resetSounds(initializeAfter: true)
+        AppCache.instance.initializeInitialScreenBackgroundTexture(screenSize: InitialScene.calculateSceneSize(self.view.frame.size))
+        
+        delay(0.3) {
+            let vc = AppDelegate.mainGameViewController
+            vc.modalTransitionStyle = .crossDissolve
+            self.present(vc, animated: true, completion: nil)
+        }
+    }
 }
